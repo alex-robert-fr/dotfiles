834c378bbb8dd5673957a2a2f6ee2419020509d4
Alex Robert <contact.alexrobert04@gmail.com>
2024-10-12 14:52:51 +0200

    🩹 Remove alpha, add pnpm and add files and directory in gitignore

--- nvim/lua/x7c00/plugins/alpha.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
-		alpha.setup(dashboard.opts)
-	end,
-}
